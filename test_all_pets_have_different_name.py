813afc3e10de2f6124b6b49705ca076963cfc729
Lily <117360193+LilyKhakimova@users.noreply.github.com>
2023-07-13 11:21:02 +0300

    test_all_pets_have_different_name.py

--- test_all_pets_have_different_name.py
+++ test_all_pets_have_different_name.py
@@ -8,26 +8,16 @@ def test_all_pets_have_different_names(go_to_my_pets):
 
    element = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".table.table-hover tbody tr")))
    # Сохраняем в переменную pet_data элементы с данными о питомцах
-   pet_data = pytest.driver.find_elements(By.CSS_SELECTOR, '.table.table-hover tbody tr')
+   name_my_pets = pytest.driver.find_elements(By.CSS_SELECTOR, '.table.table-hover tbody tr')
 
    # Настраиваем неявные ожидания:
    pytest.driver.implicitly_wait(10)
 
    # Проверяем, что у всех питомцев разные имена:
-   pets_name = []
-   for i in range(len(pet_data)):
-      data_pet = pet_data[i].text.replace('\n', '').replace('×', '')
-      split_data_pet = data_pet.split(' ')
-      pets_name.append(split_data_pet[0])
+   list_name_my_pets = []
+   for i in range(len(name_my_pets)):
+       list_name_my_pets.append(name_my_pets[i].text)
+   set_pet_data = set(list_name_my_pets)  # преобразовываем список в множество
+   assert len(list_name_my_pets) == len(set_pet_data)  # сравниваем длину списка и множества: без повторов должны совпасть
 
-   # Перебираем имена и если имя повторяется то прибавляем к счетчику r единицу.
-   # Проверяем, если r == 0 то повторяющихся имен нет.
-   r = 0
-   for i in range(len(pets_name)):
-      if pets_name.count(pets_name[i]) > 1:
-         r += 1
-   assert r == 0
-   print(r)
-   print(pets_name)
-
-# python -m pytest -v --driver Chrome --driver-path /tests_drivers/chromedriver.exe tests/test_all_pets_have_different_name.py
+# python -m pytest -v --driver Chrome --driver-path /tests/chromedriver.exe tests/test_all_pets_have_different_name.py
